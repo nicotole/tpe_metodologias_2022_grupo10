eb88b9e1e054eb4a486a53a0c42ae7d177bd6a08
Federico Pinel <42394140+fedepinel@users.noreply.github.com>
2022-06-21 17:24:42 -0300

    Se actualizo modelo de obtención de turnos

--- model/TurnosModel.php
+++ model/TurnosModel.php
@@ -11,7 +11,7 @@ class TurnosModel{
 
     public function GetTurnos(){
 
-        $sentencia = $this->db->prepare("SELECT m.nombre as nombre_medico , m.especialidad, p.nombre as nombre_paciente, t.tipo_de_turno, t.horario, t.id_turno as id_turno, t.fecha
+        $sentencia = $this->db->prepare("SELECT m.id_medico, m.nombre as nombre_medico, m.apellido as apellido_medico, m.especialidad, p.nombre as nombre_paciente, t.tipo_de_turno, t.horario, t.id_turno as id_turno, t.fecha
                                             FROM turno t
                                             JOIN medico m ON (t.id_medico = m.id_medico)
                                             JOIN paciente p ON (t.dni = p.dni) AND (t.nro_afiliado = p.nro_afiliado)
